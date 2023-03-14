6c7040fcf449332b5bc902a988a7abb667e19c5e
Aftab Alam <alamaftab413@gmail.com>
2023-03-14 19:03:18 +0500

    .
    .

--- controllers/admin/getTeacher.js
+++ controllers/admin/getTeacher.js
@@ -1,17 +1,28 @@
 const Teacher = require('../../data-modals/user-models/teacher-model');
 
 module.exports.getTeacherQuery = async function (req, res) {
+    let availableTeachersAndSlots = { availableTeachers: [], availableDaysAndSlots: [] };
     try {
         if (!req.query.slot && !req.query.day && req.query.subject) {
             req.query.subject.toLowerCase();
-            res.send(await Teacher.find({ subjects: { $in: [req.query.subject] } }).populate('userInfo'));
+            const teachers = await Teacher.find({ subjects: { $in: [req.query.subject] } }).populate('userInfo');
+            for (teacher of teachers) {
+                availableTeachersAndSlots.availableTeachers.push({
+                    fullName: teacher.userInfo.fullName,
+                    _id: teacher.userInfo._id,
+                    gender: teacher.gender,
+                    address: teacher.address
+                });
+                availableTeachersAndSlots.
+            }
+            res.send(availableTeachers);
         } else if (!req.query.slot && req.query.day && req.query.subject) {
             req.query.subject.toLowerCase();
             res.send(await Teacher.find({ subjects: { $in: [req.query.subject] }, availability: { $elemMatch: { name: req.query.day } }, }).populate('userInfo'));
         }
         else if (req.query.slot && req.query.subject && req.query.day) {
+            let availableTeachers;
             let allteachers = await Teacher.find({ subjects: { $in: [req.query.subject] } }).populate('userInfo');
-            let availableTeachers = [];
             for (teacher of allteachers) {
                 for (avail of teacher.availability) {
                     if (avail.name == req.query.day && avail.slot.includes(req.query.slot)) {
