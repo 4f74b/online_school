6c919aab98f5d010d646f105369b062454e787f5
Aftab Alam <alamaftab413@gmail.com>
2023-03-20 23:01:07 +0500

    Update app.js

--- app.js
+++ app.js
@@ -30,7 +30,7 @@ const ExpressError = require('./utils/ExpressError');
 // load environment vairables
 dotEnv.config();
 const app = express();
-const port = process.env.PORT || 3000;
+const port = process.env.PORT || 80;
 
 
 
