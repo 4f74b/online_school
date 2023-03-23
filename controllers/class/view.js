1de46a16584cbf63ec8d670db4009f3ac88f268c
Aftab Alam <alamaftab413@gmail.com>
2023-03-23 16:12:43 +0500

    .
    .

--- controllers/class/view.js
+++ controllers/class/view.js
@@ -43,8 +43,45 @@ module.exports.viewInteractiveClass = async function (req, res) {
             ],
         },
     ]);
+    cls.table = generateScheduleTable(cls.courses);
+    console.log(cls.table);
     res.render('class/overview-interactive-class', { cls });
 }
+
+
+
+function generateScheduleTable(data) {
+    const daysOfWeek = ['monday', 'tuesday', 'wednesday', 'thursday', 'friday', 'saturday', 'sunday'];
+
+    // Create a mapping of subjects to their schedule
+    const scheduleMap = {};
+    data.forEach(subject => {
+        const schedule = subject.schedule;
+        schedule.forEach(sched => {
+            const day = sched.day;
+            const slot = sched.slot;
+            if (!scheduleMap[subject.name]) {
+                scheduleMap[subject.name] = {};
+            }
+            scheduleMap[subject.name][day] = slot;
+        });
+    });
+
+    // Create the array of objects
+    const scheduleTable = [];
+    scheduleTable.push(['', ...daysOfWeek]); // Add header row
+    Object.keys(scheduleMap).forEach(subject => {
+        const row = [subject];
+        daysOfWeek.forEach(day => {
+            const slot = scheduleMap[subject][day];
+            row.push(slot || '-');
+        });
+        scheduleTable.push(row);
+    });
+
+    return scheduleTable;
+}
+
 
 
 module.exports.viewAllClasses = async function (req, res) {
