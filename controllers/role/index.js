2fc7a7958647eef2911e3b19fad5cab199a9a050
Aftab Alam <alamaftab413@gmail.com>
2023-03-24 22:06:49 +0500

    .
    .

--- controllers/role/index.js
+++ controllers/role/index.js
@@ -1,6 +1,28 @@
 module.exports.isTeacher = async function (req, res, next) {
     if (req.user) {
-        if (req.user.isTeacher) {
+        if (req.user.role == 'teacher') {
+            next();
+        } else {
+            res.render('error/error');
+        }
+    } else {
+        res.send('Not Logged in');
+    }
+}
+module.exports.isAdmin = async function (req, res, next) {
+    if (req.user) {
+        if (req.user == 'admin') {
+            next();
+        } else {
+            res.render('error/error');
+        }
+    } else {
+        res.send('Not Logged in');
+    }
+}
+module.exports.isStudent = async function (req, res, next) {
+    if (req.user) {
+        if (req.user == 'student') {
             next();
         } else {
             res.render('error/error');
