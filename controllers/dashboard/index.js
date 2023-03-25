2afe2bb230b332bc088eb21fc788230c4337c0e1
Aftab Alam <alamaftab413@gmail.com>
2023-03-25 17:41:11 +0500

    .
    .

--- controllers/dashboard/index.js
+++ controllers/dashboard/index.js
@@ -2,18 +2,7 @@ const mongoose = require('mongoose');
 const User = require('../../data-modals/user');
 
 module.exports.showStudentDashboard = async function (req, res) {
-    // const student = await User.findById(req.user._id)
-    //     .populate([
-    //         {
-    //             path: 'userProfile',
-    //             populate: {
-    //                 path: 'admittedClass',
-    //                 populate: {
-    //                     path: 'courses', populate: [{ path: 'assignment' }, { path: 'material' }], select: ['-students']
-    //                 }
-    //             }
-    //         }
-    //     ])
-    // console.log(student)
+    // Note=====================
+    // res.locals fills all information about student
     res.render('student/dashboard');
 }
